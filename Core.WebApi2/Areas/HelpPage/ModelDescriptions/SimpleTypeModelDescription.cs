3692544da3448ad18265dc217fbf13d466fcae8e
sunilppandey <sunilppandey@gmail.com>
2015-12-12 09:25:33 +0530

    Deleted projects removed.

--- Core.WebApi2/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Core.WebApi2.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- Core.WebApi2/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Core.WebApi2.WebApiApplication" Language="C#" %>
