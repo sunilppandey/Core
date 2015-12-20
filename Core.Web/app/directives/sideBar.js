bfe592e2dc1078c4a10b4577e097d3d85485b530
sunilppandey <sunilppandey@gmail.com>
2015-12-20 17:48:57 +0530

    New theme added

---
+++ Core.Web/app/directives/sideBar.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+app.directive('sideBar', function() {
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl:'app/views/layout/sideBar.html'
+    }
+});
+
+//(angular.module('common.ui'))
---
+++ Core.Web/app/directives/topBar.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+app.directive('topBar', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: 'app/views/layout/topBar.html'
+    }
+});
